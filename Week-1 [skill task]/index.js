7a63c55b8a0fe4a903310383866c6971144386e5
Mukesh Choudhary <mukeshchoudhary33333@gmail.com>
2024-08-06 18:18:26 +0530

    Added functionality

---
+++ Week-1 [skill task]/index.js
@@ -0,0 +1,10 @@
+// const api_url ="https://stoic.tekloon.net/stoic-quote";
+// const quote = document.querySelectorAll(".quote");
+// async function getapi(url)
+// {
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   console.log(data)
+// }
+
+// getapi(api_url);
